764a05b36bf45edfbcb077f07f6193f1bf14bad9
Headliner770 <paul248022@gmail.com>
2025-10-29 18:56:04 +0300

    step-15 + step-16 + step-17

---
+++ Course1/step-17.js
@@ -0,0 +1,6 @@
+// Step 17
+// ❓ You can also perform subtraction (-), multiplication (*), and division (/). Feel free to experiment with the operators and numbers in your console.log. When you are ready to move on, remove the console.log.
+
+// ✔️ SOLUTION
+
+let count = 8;
