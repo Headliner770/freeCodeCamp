8e7b26e296fa012fafff99af42f986a8cae28967
Headliner770 <paul248022@gmail.com>
2025-10-29 13:32:07 +0300

    step-5

---
+++ Course1/step-5.js
@@ -0,0 +1,18 @@
+// Step 5
+// The console allows you to print and view JavaScript output. You can send information to the console using console.log(). For example, this code will print "Naomi" to the console:
+// Консоль позволяет выводить и просматривать вывод JavaScript. Вы можете отправить информацию в консоль с помощью console.log(). Например, этот код выведет на консоль слово «Naomi»:
+
+// Example Code
+// let developer = "Naomi";
+// console.log(developer);
+
+// The code above accesses the developer variable with its name in the console.log(). Note that the value between the parentheses is the value that will be printed.
+// Приведённый выше код обращается к переменной разработчика по её имени в console.log(). Обратите внимание, что значение в скобках — это значение, которое будет выведено на экран.
+
+// Print the value of the character variable to the console. Then, click the "Console" button to view the JavaScript console.
+// Выведите значение символьной переменной на консоль. Затем нажмите кнопку «Консоль», чтобы открыть консоль JavaScript.
+
+// ✔️ SOLUTION
+
+let character = "Hello";
+console.log(character);
