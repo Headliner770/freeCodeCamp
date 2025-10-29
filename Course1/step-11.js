32ce433c1fa476e9b50916ed88e8056a850dca85
Headliner770 <paul248022@gmail.com>
2025-10-29 18:33:37 +0300

    step-11 + step-12

---
+++ Course1/step-11.js
@@ -0,0 +1,20 @@
+// Step 11
+// You can also assign the value of a variable to another variable. For example:
+// Вы также можете присвоить значение одной переменной другой переменной
+
+// Example Code
+// let first = "One";
+// let second = "Two";
+// second = first;
+// The second variable would now have the value "One".
+// Вторая переменная теперь будет иметь значение «Один»
+
+// ❓ To see this in action, change your secondCharacter assignment from "Test" to your character variable.
+// Чтобы увидеть это в действии, измените присвоение переменной secondCharacter с "Test" на вашу переменную character
+
+// Then open the console to see what gets logged.
+
+// ✔️ SOLUTION
+
+secondCharacter = "Test";
+secondCharacter = character;
