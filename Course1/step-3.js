a78b9471d683e5cc4210fe7225a0201f2f35b645
Headliner770 <paul248022@gmail.com>
2025-10-29 13:28:24 +0300

    step-4

--- Course1/step-3.js
+++ Course1/step-3.js
@@ -5,7 +5,7 @@
 // let hello = "Hello";
 // Assigning a value to a variable at the moment of its declaration is known as initialization.
 
-// Initialize your character variable by assigning it the value "Hello" during its declaration.
+// ❓ Initialize your character variable by assigning it the value "Hello" during its declaration.
 
 // ✔️ SOLUTION
 
