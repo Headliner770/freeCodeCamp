eccd5ae7d596a56278c5a6a48b7e1512ef98e4ce
Headliner770 <paul248022@gmail.com>
2025-10-29 13:21:20 +0300

    Rename task,  step2

---
+++ Course1/step2.js
@@ -0,0 +1,18 @@
+// Step 2
+// One of the most important concepts in programming is variables. A variable points to a specific memory address that stores a value. Variables are given a name which can be used throughout your code to access that value.
+// Переменные — одно из важнейших понятий в программировании. Переменная указывает на определённый адрес памяти, где хранится значение. Переменным присваивается имя, которое можно использовать в коде для доступа к этому значению.
+
+// Declaring a variable means giving it a name. In JavaScript, this is often done with the let keyword. For example, here is how you would declare a hello variable:
+
+// Example Code
+// let hello;
+// Variable naming follows specific rules: names can include letters, numbers, dollar signs, and underscores, but cannot contain spaces and must not begin with a number.Имена переменных следуют определенным правилам: имена могут включать буквы, цифры, знаки доллара и подчеркивания, но не могут содержать пробелы и не должны начинаться с цифры.
+
+// Use the let keyword to declare a variable called character.
+// Используйте ключевое слово let для объявления переменной с именем character.
+
+// Note: It is common practice to end statements in JavaScript with a semicolon. ;
+
+// ✔️ SOLUTION
+
+let character;
