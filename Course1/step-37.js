602409921948461cf89d87c1c2c487b7aa2079a8
Headliner770 <paul248022@gmail.com>
2025-10-31 11:11:58 +0300

    step-35 + 36 + 37 + 38

---
+++ Course1/step-37.js
@@ -0,0 +1,8 @@
+// Step 37
+// Your loop should now run eight times. Inside the body of the loop, print the value of the i iterator and see what happens.
+
+// ✔️ SOLUTION
+
+for (let i = 0; i < count; i = i + 1) {
+  console.log(i);
+}
---
+++ Course1/step-38.js
@@ -0,0 +1,12 @@
+// Step 38
+// You should see the numbers zero through seven printed in your console, one per line. This will serve as the foundation for generating your pyramid.
+
+// Replace your log statement with a statement to push i to your rows array.
+
+// ✔️ SOLUTION
+const count = 8;
+const rows = [];
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+console.log(rows);
