66dc1d48619208958b6224cd0dadd82f3fb331a5
Headliner770 <paul248022@gmail.com>
2025-10-31 09:34:07 +0300

    step-22 + step-23 + step-24

---
+++ Course1/step-23.js
@@ -0,0 +1,6 @@
+// Step 23
+// For now, remove your first console log and your rows[rows.length - 1] assignment. Leave the second rows log statement for later.
+
+// ✔️ SOLUTION
+
+console.log(rows);
